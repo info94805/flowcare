e65435546cba01ffe0535bef14c6c9af3b7dba35
Yash Soni <infy.business@gmail.com>
2026-05-28 04:26:50 +0000

    File changes

--- src/components/SplashScreen.jsx
+++ src/components/SplashScreen.jsx
@@ -1,8 +1,6 @@
 import React, { useEffect } from 'react';
 import { motion } from 'framer-motion';
 
-const LOGO_URL = "https://media.base44.com/images/public/6a16792f97578664742ab8d9/89f0caaaa_79e53f3f8_logo.png";
-
 export default function SplashScreen({ onDone }) {
   useEffect(() => {
     const t = setTimeout(() => onDone(), 3200);
@@ -33,11 +31,7 @@ export default function SplashScreen({ onDone }) {
           animate={{ opacity: 1, scale: 1, y: 0 }}
           transition={{ type: 'spring', stiffness: 180, damping: 16, duration: 0.8 }}
         >
-          <img
-            src={LOGO_URL}
-            alt="FlowCare Logo"
-            className="w-36 h-36 object-contain drop-shadow-xl"
-          />
+          <div className="text-8xl drop-shadow-xl">🌸</div>
         </motion.div>
 
         {/* App name */}
